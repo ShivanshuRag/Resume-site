d86d40cbdf7a9a9d8319d69f637ef0c113447e08
ShivanshuRag <ragsingh773@gmail.com>
2025-01-15 16:44:49 +0530

    add and some change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,39 +1,68 @@
 // src/pages/About.jsx
-// import React from 'react';
+
+
+import profileImage from '../assets/shivanshu1.jpg'; // Replace with your image path
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'; 
 
 const About = () => {
   return (
-    <div className="container mx-auto p-16 text-slate-300">
-      <h1 className="text-3xl font-bold mb-4">About Me</h1>
-      <p className="text-lg mb-8">
+    <div className="container mx-auto my-auto p-16 flex flex-col md:flex-row items-center justify-between">
+      <div className="md:w-1/2 text-slate-300">
+        <h1 className="text-3xl font-bold mb-4">About Me</h1>
+        <p className="text-lg mb-8">
+          I am Shivanshu Singh, a passionate software developer with a strong background in web development. I have experience working with various front-end and back-end technologies, and I enjoy creating user-friendly and efficient applications. I am always eager to learn new skills and improve my existing ones. In my free time, I like to read books, explore new technologies, and stay updated with the latest trends in the tech industry.
+          I am also a strong advocate for clean code and best practices in software development. I believe in writing maintainable and scalable code that can be easily understood and extended by other developers. Collaboration and teamwork are essential to me, and I enjoy working in dynamic environments where I can contribute to the success of the team and the project.
+        </p>
+        <h2 className="text-2xl font-semibold mb-2">Certifications</h2>
+        <ul className="list-disc ml-4">
+          <li>Full Stack Developer - PW (PhysicsWallah)</li>
+          <li>Chai aur code - Hitesh Chaudhary</li>
+        </ul>
+        <h2 className="text-2xl font-semibold mb-2 mt-4">Skills</h2>
+        <ul className="list-disc ml-4">
+          <li>HTML, CSS, JavaScript, typescript, Java</li>
+          <li>React-vite, Next.js, vue.js</li>
+          <li>Tailwind CSS, Bootstrap</li>
+          <li>Git, GitHub, Linux</li>
+          {/* Add more skills as needed */}
+        </ul>
+        <h2 className="text-2xl font-semibold mb-2 mt-4">Education</h2>
+        <p>
+          Institute of Engineering and Rural Technology ( IERT ) College, <br />
+          Prayagraj, Uttar Pradesh <br />
+          Mechanical Engineering <br />
+          
+        </p>
         
-        I am Shivanshu Singh,
-        I am a passionate software developer with a strong background in web development. I have experience working with various front-end and back-end technologies, and I enjoy creating user-friendly and efficient applications. I am always eager to learn new skills and improve my existing ones. In my free time, I like to read books, explore new technologies, and stay updated with the latest trends in the tech industry.
-        I am also a strong advocate for clean code and best practices in software development. I believe in writing maintainable and scalable code that can be easily understood and extended by other developers. Collaboration and teamwork are essential to me, and I enjoy working in dynamic environments where I can contribute to the success of the team and the project.
-      </p>
-      {/* Add a section for skills, e.g., */}
-      <h2 className="text-2xl font-semibold mb-2"> Certifications</h2>
-         <h3 > Full Stack Developer - PW (PhysicsWallah)</h3>
-         <h3>Chai aur code - Hitesh Chaudhary</h3>
-           
-      <h2 className="text-2xl font-semibold mb-2 mt-4">Skills</h2>
-      <ul className="list-disc ml-4">
-        <li>HTML, CSS, JavaScript , java </li>
-        <li>React-vite, Next.js, vue.js </li>
-        <li>Tailwind CSS, Bootstrap</li>
-       
-        <li>Git, GitHub ,Linux</li>
-        {/* Add more skills as needed */}
-      </ul>
-
-      <h2 className="text-2xl font-semibold mb-2 mt-4">Education</h2>
-       <h3> 
-                Institute of Engineering and Rural Technology ( IERT ) College, <br />
-                Prayagraj , UttarPradesh <br />
-                Mechanical Engineering <br />
-                2022 - 2025
-        </h3>
+      </div>
+      <div className="md:w-1/2 flex justify-center items-center">
+        <img
+          src={profileImage}
+          alt="Shivanshu Singh"
+          className="w-48 h-48 rounded-md object-cover shadow-lg"
+          style={{
+            // Fine-tune object-position for better face focus
+            objectPosition: '50% 10%', 
+            // Optional: Add a subtle blur to the background
+          
+          }}
+        />
+        <div className="flex mt-72  space-x-4"> 
+            
+          <a href="https://github.com/ShivanshuSingh01" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-gray-400 hover:text-gray-500 text-2xl" />
+          </a>
+          <a href="https://www.linkedin.com/in/shivanshu-singh-9666a529b/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-gray-400 hover:text-gray-500 text-2xl" />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-gray-400 hover:text-gray-500 text-2xl" />
+          </a>
+        </div>
+      </div>
+    
     </div>
+    
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,69 @@
 // import  { Suspense } from 'react';
 import { Link } from 'react-router-dom';
-import Images1 from "../assets/yoga.png"; 
+import Images1 from "../assets/shivanshu1.jpg"; 
 import Spline from '@splinetool/react-spline';
 import { TypeAnimation } from 'react-type-animation'
 const Home = () => {
-  return (
+return (
     <div 
-      style={{ 
-        position: 'relative', 
-        overflow: 'hidden' 
-      }}
+        style={{ 
+            position: 'relative', 
+            overflow: 'hidden' 
+        }}
     >
- 
-
-      
-        <Spline 
-          scene="https://prod.spline.design/882sMsYSAQN-A82L/scene.splinecode" 
-          style={{ 
-            position:"absolute", 
-            top: 0, 
-            left: 0, 
-            width: '100%', 
-            height: '100%', 
-            zIndex: -1 
-          }} 
-        /> 
+            <Spline 
+                scene="https://prod.spline.design/882sMsYSAQN-A82L/scene.splinecode" 
+                style={{ 
+                    position:"absolute", 
+                    top: 0, 
+                    left: 0, 
+                    width: '100%', 
+                    height: '100%', 
+                    zIndex: -1 
+                }} 
+            /> 
     
-
-      <div className="container mx-auto py-16 flex flex-col items-center justify-center"> 
-        <div className="relative overflow-hidden rounded-lg shadow-lg"> 
-          <img 
-            src={Images1} 
-            alt="Your Photo" 
-            className="w-48 h-48 rounded-full object-cover" 
-          /> 
-          {/* <div className="absolute inset-0 bg-gray-900 opacity-0 hover:opacity-50 rounded-full transition-opacity duration-300"></div>  */}
-        </div> 
-        <h1 className="text-4xl font-bold text-slate-400 mt-8">
-            
-            
-
-           
-      
-    
-                <TypeAnimation
-                sequence={[
-                    // Same substring at the start will only be typed out once, initially
-                    `Hello! I am Shivanshu Singh`,
-                    1000, // wait 1s before replacing "Mice" with "Hamsters"
-                    `Namaste! I am Shivanshu Singh`,
-                    1000,
-                    
-                ]}
-                wrapper="span"
-                speed={30}
-                style={{  display: 'inline-block', color: '#D3D3D3' , filter: "drop-shadow(0px 10px 10px rgb(0,0,0))" }}
-                repeat={Infinity}
-                className="font-effect-3d"
-                />
-       
+        <div className="container mx-auto py-16 flex flex-col items-center justify-center"> 
+        <div className="relative overflow-hidden rounded-md shadow-lg">
+      <img
+        src={Images1}
+        alt="Your Photo"
+        className="w-48 h-48 object-cover  duration-300 transform hover:scale-105 hover:translate-y-[-5px]"
+        style={{
+          // Fine-tune object-position for better face focus
+          objectPosition: '50% 10%', 
+          // Optional: Add a subtle blur to the background
+        
+        }}
+      />
+    </div> 
+            <h1 className="text-4xl font-bold text-slate-400 mt-8">
+                    <TypeAnimation
+                            sequence={[
+                                    `Hello! I am Shivanshu Singh`,
+                                    1000,
+                                    `Namaste! I am Shivanshu Singh`,
+                                    1000,
+                            ]}
+                            wrapper="span"
+                            speed={30}
+                            style={{  display: 'inline-block', color: '#D3D3D3' , filter: "drop-shadow(0px 10px 10px rgb(0,0,0))" }}
+                            repeat={Infinity}
+                            className="font-effect-3d"
+                    />
             </h1> 
-        <p className="text-lg text-slate-400 mt-4"> 
-        I’m a <span className="text-teal-500 text-xl font-bold">Fullstack Developer</span> and I’ve always been passionate about technology and education.  
-           
-        </p> 
-        <Link 
-          to="/projects" 
-          className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded mt-8" 
-        > 
-          View My Projects 
-        </Link> 
-      </div> 
+            <p className="text-lg text-slate-400 mt-4"> 
+                I’m a <span className="text-teal-500 text-xl font-bold">Fullstack Developer</span> and I’ve always been passionate about technology and education.  
+            </p> 
+            <Link 
+                to="/projects" 
+                className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded mt-8" 
+            > 
+                View My Projects 
+            </Link> 
+        </div> 
     </div>
-  );
+);
 };
 
 export default Home;
